d5e783b03f34fa4236e0b0b4ebd35ad5175db818
ayhantefik <ayhantefik93@gmail.com>
2024-11-21 18:40:52 +0100

    vibrations module

--- M3inl.ino
+++ M3inl.ino
@@ -1,9 +1,39 @@
+#define BTN 7
+#define VIB 4
+
+// Jag använder "16. vibrationssensormodul"
+
 void setup() {
-    // put your setup code here, to run once:
-    
+    pinMode(BTN, INPUT_PULLUP);
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(VIB, INPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.begin(9600);
 }
 
+int btnValue = LOW;
+int ledValue = HIGH;
+
 void loop() {
-    // put your main code here, to run repeatedly:
-    
+    int vibValue = digitalRead(VIB);
+    if(digitalRead(BTN) == LOW){
+      if(btnValue == HIGH){
+        btnValue = LOW;
+        digitalWrite(LED_BUILTIN, HIGH);
+        Serial.println("OFF");
+      }
+      else{
+        btnValue = HIGH;
+        digitalWrite(LED_BUILTIN, LOW);
+        Serial.println("ON");
+      }
+    }
+    if(btnValue == HIGH){
+      if(vibValue == LOW)
+        Serial.println("No vibration");
+      else
+        Serial.println("Warning: VIBRATION");
+    }
+
+    delay(100);
 }
